141172f93caf94e9e9a9371b6155c4b81cd15c60
Yuvraj Tayal <yuvrajtayal1202@gmail.com>
2025-05-18 02:03:22 +0530

    Bookstore First HTTP request

--- BookStore/backend/index.js
+++ BookStore/backend/index.js
@@ -1,6 +1,12 @@
-import {express} from 'express'
-import {PORT} from './config'
-const app = express()
+import express from 'express'
+import {PORT} from './config.js'
+
+const app = express();
+
+app.get('/', (req, res) =>{
+    console.log(req)
+    return res.status(234).send('Welcome TO the the bopok store')
+})
 
 app.listen(PORT, ()=>{
     console.log(`App is listening on ${PORT}`)
