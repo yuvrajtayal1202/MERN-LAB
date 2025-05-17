61a8b39ada9a2f3e064b14a86ae4852000504b24
Yuvraj Tayal <yuvrajtayal1202@gmail.com>
2025-05-18 03:47:53 +0530

    BookStore Frontend startd

--- BookStore/backend/index.js
+++ BookStore/backend/index.js
@@ -1,18 +1,27 @@
 import express from "express";
 import mongoose from "mongoose";
 import { PORT, mongoURL } from "./config.js";
-import booksRoute from './routes/booksRoute.js'
+import booksRoute from "./routes/booksRoute.js";
 const app = express();
 
 app.use(express.json());
 
-app.use('/books', booksRoute);
+app.use("/books", booksRoute);
+
+app.use(cors());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.get("/", (req, res) => {
   console.log(req);
   return res.status(234).send("Welcome TO the the bopok store");
 });
-
 
 mongoose
   .connect(mongoURL)
