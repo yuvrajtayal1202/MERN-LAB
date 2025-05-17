a118c600352b4b520ff5ac6caa29bbdf38bc0810
Yuvraj Tayal <yuvrajtayal1202@gmail.com>
2025-05-18 03:13:40 +0530

    BookStore Get one book by ID

--- BookStore/backend/index.js
+++ BookStore/backend/index.js
@@ -4,49 +4,56 @@ import { PORT, mongoURL } from "./config.js";
 import { Book } from "./models/bookModel.js";
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 app.get("/", (req, res) => {
   console.log(req);
   return res.status(234).send("Welcome TO the the bopok store");
 });
 
-app.post('/books', async (req, res)=>{
-try {
-    if(
-        !req.body.title || !req.body.author || !req.body.publishYear
-    )
-    {
-        return res.status(400).send({
-            message: 'Send all the required feilds: title, author, publishYear'
-        })
+app.post("/books", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      return res.status(400).send({
+        message: "Send all the required feilds: title, author, publishYear",
+      });
     }
     const newBook = {
-        title: req.body.title,
-        author: req.body.author,
-        publishYear: req.body.publishYear
-    }
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
 
-    const book = await Book.create(newBook)
-    res.status(201).send(book)
-} catch (error) {
-    console.log(error)
-   return  res.status(500).send({message: error.message})
-}
-})
+    const book = await Book.create(newBook);
+    res.status(201).send(book);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ message: error.message });
+  }
+});
 
-app.get('/books', async (req, res) =>{
-    try {
-        const books = await Book.find({});
-        return res.status(200).json({
-            count: books.length,
-            data: books
-        })
-    } catch (error) {
-        console.log(error)
-        res.status(500)
-    }
-})
+app.get("/books", async (req, res) => {
+  try {
+    const books = await Book.find({});
+    return res.status(200).json({
+      count: books.length,
+      data: books,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+  }
+});
+app.get("/books/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const book = await Book.findById(id);
+    return res.status(200).send(book);
+  } catch (error) {
+    console.log(error);
+    res.status(500);
+  }
+});
 
 mongoose
   .connect(mongoURL)
