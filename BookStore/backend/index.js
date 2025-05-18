938fa69f9ede1789a026deebd3f21778c301c906
Yuvraj Tayal <yuvrajtayal1202@gmail.com>
2025-05-18 16:24:19 +0530

    BookStore bugs

--- BookStore/backend/index.js
+++ BookStore/backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 
 // CORS middleware must come BEFORE routes!
-app.use(cors())
+// app.use(cors())
 app.use(cors({
   origin: 'https://book-store-tan-one.vercel.app', // your frontend URL
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
