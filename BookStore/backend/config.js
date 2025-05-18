6c789863be9115b06094eb6f30c1897ed49f45d6
Yuvraj Tayal <yuvrajtayal1202@gmail.com>
2025-05-18 15:10:40 +0530

    BookStore Backend Updated

--- BookStore/backend/config.js
+++ BookStore/backend/config.js
@@ -1,4 +1,4 @@
-export const PORT  = 5555;
+export const PORT = process.env.PORT || 5555;
 
-export const mongoURL  = 
-'mongodb+srv://user:user@bookstore.jdc6esa.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStore;'
+export const mongoURL =
+  'mongodb+srv://user:user@bookstore.jdc6esa.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStore;';
